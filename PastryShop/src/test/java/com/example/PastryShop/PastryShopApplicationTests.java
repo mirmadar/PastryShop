ad0baca4c40cbef208e5ff0dcfe946c6e481b7a4
mirmadar <121007143+mirmadar@users.noreply.github.com>
2023-12-05 11:43:17 +0300

    add project files

---
+++ PastryShop/src/test/java/com/example/PastryShop/PastryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PastryShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
